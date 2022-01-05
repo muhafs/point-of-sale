55925304fe068697aebcad4fac8e0ae52ad84518
muhamadhaspin <muhamadhaspin@gmail.com>
2022-01-05 11:18:18 +0700

    modify dashboard view folders

--- mypos/app/Http/Controllers/DashboardController.php
+++ mypos/app/Http/Controllers/DashboardController.php
@@ -15,7 +15,7 @@ class DashboardController extends Controller
      */
     public function index()
     {
-        return view('dashboard.index');
+        return view('admin.index');
     }
 
     /**
