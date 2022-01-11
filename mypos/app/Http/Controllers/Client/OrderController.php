5145fd5ad6e36744b74ae7f6f8c95b6edd53ae83
muhamadhaspin <muhamadhaspin@gmail.com>
2022-01-11 18:05:51 +0700

    make (store & update) method code more Cleaner

--- mypos/app/Http/Controllers/Client/OrderController.php
+++ mypos/app/Http/Controllers/Client/OrderController.php
@@ -33,32 +33,8 @@ class OrderController extends Controller
             'products' => 'required|array',
         ]);
 
-        // Create an Empty Order of Current Client
-        $order = $client->orders()->create([]);
-
-        // Create Pivot (product_order) with a given values
-        $order->products()->attach($request->products);
-
-        // Set Total Price
-        $total_price = 0;
-        // Set
-        foreach ($request->products as $id => $quantity) {
-            // Get the Current Product
-            $product = Product::findOrFail($id);
-
-            // Insert Current Product's Total Price into (total_price) Variable
-            $total_price += $product->sale_price * $quantity['quantity'];
-
-            // Decrement the Stock of Current Product
-            $product->update([
-                'stock' => $product->stock - $quantity['quantity']
-            ]);
-        }
-
-        // Set Total Price of the Order
-        $order->update([
-            'total_price' => $total_price
-        ]);
+        //? Create the New Order
+        $this->attachOrder($request, $client);
 
         return redirect('orders')->with('success', 'Order has been added successfully');
     }
@@ -78,18 +54,18 @@ class OrderController extends Controller
             'products' => 'required|array',
         ]);
 
-        //! Delete the Exist Order
-        // Increase The product's stock Before Deleting the Order
-        foreach ($order->products as $product) {
-            $product->update([
-                'stock' => $product->stock + $product->pivot->quantity,
-            ]);
-        }
+        //? Delete the Exist Order
+        $this->attachOrder($request, $client);
 
-        // Delete the order
-        $order->delete();
+        //? Create the New Order
+        $this->detachOrder($order);
 
-        //! Create the New Order
+        return redirect('orders')->with('success', 'Order has been updated successfully');
+    }
+
+    //! EXTRA METHODS TO MAKE CODE MORE CLEANER
+    private function attachOrder(Request $request, Client $client)
+    {
         // Create an Empty Order of Current Client
         $order = $client->orders()->create([]);
 
@@ -116,13 +92,18 @@ class OrderController extends Controller
         $order->update([
             'total_price' => $total_price
         ]);
-
-        return redirect('orders')->with('success', 'Order has been updated successfully');
     }
 
-    //! DESTROY
-    public function destroy(Client $client, Order $order)
+    private function detachOrder(Order $order)
     {
-        //
+        // Increase The product's stock Before Deleting the Order
+        foreach ($order->products as $product) {
+            $product->update([
+                'stock' => $product->stock + $product->pivot->quantity,
+            ]);
+        }
+
+        // Delete the order
+        $order->delete();
     }
 }
